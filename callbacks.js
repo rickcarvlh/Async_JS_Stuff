0e1fafca4e1fe4b93b37dd2fc42af982df1ffbcb
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-03-25 14:53:28 +0000

    updating files

---
+++ callbacks.js
@@ -0,0 +1,18 @@
+const post = [
+    {
+        title: 'Post One', body: 'This is post one'
+    },
+    {
+        title: 'Post Two', body: 'This is post two'
+    }
+];
+
+
+//  mimic server 
+// hign order function - funçao dentro de uma funçao
+//  * funçao que chama outra funçao
+function getPosts() {
+    setTimeout(() => {
+        
+    }, 1000);
+}
