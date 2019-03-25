89daa145856d9dd9a2b94eeae25ac1f982255f74
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-03-25 16:02:37 +0000

    promises.js

--- callbacks.js
+++ callbacks.js
@@ -1,4 +1,4 @@
-const post = [
+const posts = [
     {
         title: 'Post One', body: 'This is post one'
     },
@@ -14,11 +14,24 @@ const post = [
 function getPosts() {
     setTimeout(() => {
         let output = "";
-        post.forEach((post, index) =>{
+        posts.forEach((post, index) =>{
             output += `<li>${post.title}</li>`;
         });
         document.body.innerHTML = output;
     }, 1000);
 }
 
-getPosts();
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+// getPosts();
+
+// * chamada logo se seguida callback function
+createPost({title: 'Post Three', body: 'This is post three' }, getPosts);
+
+// console.log(posts);
+
